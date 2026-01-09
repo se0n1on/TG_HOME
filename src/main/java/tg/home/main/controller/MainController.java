d1e5ba589b7f068b3cf8bdee2bb5f9b5a85d322f
seoeon <seoeon@tgin.kr>
2026-01-09 11:15:08 +0900

    renew

--- src/main/java/tg/home/main/controller/MainController.java
+++ src/main/java/tg/home/main/controller/MainController.java
@@ -25,28 +25,28 @@ public class MainController {
 		model.addAttribute("clients", mainService.getClients());
 		model.addAttribute("contact", mainService.getContactInfo());
 
-		return "main/main";
+		return "index";
 	}
 
-	@GetMapping("/about")
-	public String about(Model model) {
-		model.addAttribute("companyInfo", mainService.getCompanyInfo());
-		model.addAttribute("values", mainService.getCompanyValues());
-		model.addAttribute("history", mainService.getCompanyHistory());
-		return "main/about";
-	}
-
-	@GetMapping("/services")
-	public String services(Model model) {
-		model.addAttribute("services", mainService.getServices());
-		model.addAttribute("projects", mainService.getProjects());
-		return "main/services";
-	}
-
-	@GetMapping("/contact")
-	public String contact(Model model) {
-		model.addAttribute("leadership", mainService.getLeadershipTeam());
-		model.addAttribute("contact", mainService.getContactInfo());
-		return "main/contact";
-	}
+//	@GetMapping("/about")
+//	public String about(Model model) {
+//		model.addAttribute("companyInfo", mainService.getCompanyInfo());
+//		model.addAttribute("values", mainService.getCompanyValues());
+//		model.addAttribute("history", mainService.getCompanyHistory());
+//		return "main/about";
+//	}
+//
+//	@GetMapping("/services")
+//	public String services(Model model) {
+//		model.addAttribute("services", mainService.getServices());
+//		model.addAttribute("projects", mainService.getProjects());
+//		return "main/services";
+//	}
+//
+//	@GetMapping("/contact")
+//	public String contact(Model model) {
+//		model.addAttribute("leadership", mainService.getLeadershipTeam());
+//		model.addAttribute("contact", mainService.getContactInfo());
+//		return "main/contact";
+//	}
 }
