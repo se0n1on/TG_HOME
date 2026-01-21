80f1841a380d93819918416f627f1169390bf736
seoeon <seoeon@tgin.kr>
2026-01-21 13:24:36 +0900

    tomcat distribute settings

--- src/main/java/tg/home/com/config/CachedBodyHttpServletRequest.java
+++ src/main/java/tg/home/com/config/CachedBodyHttpServletRequest.java
@@ -1,9 +1,9 @@
 package tg.home.com.config;
 
-import javax.servlet.ReadListener;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
